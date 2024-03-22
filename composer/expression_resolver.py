bfa6c5e174f6a4a9b456b83ed04b98f46f67899c
İbrahim Sel <ibrahim.sel@eteration.com>
2024-03-22 15:12:31 +0300

    initialize ExpressionResolver class

---
+++ composer/expression_resolver.py
@@ -0,0 +1,15 @@
+class ExpressionResolver:
+    """Resolve Muto expressions like find, arg, etc."""
+
+    def __init__(self):
+        pass
+
+    def has_expression(self, value):
+        """
+        Returns True if the value contains a Muto Expression
+        Returns False otherwise
+        """
+        pass
+
+    def resolve_expression(self, value):
+        pass
